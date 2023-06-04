cdbefa99ad68f5e49ac203162d28900e6fb0674a
Isael <isaelgsfaria@gmail.com>
2023-06-04 00:07:16 -0300

    Added level node and scaled window

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,10 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * 0.75
 
 func _physics_process(delta):
 	# Add gravity.
